c3f54dfe431ec995463dcf811f9d90bef81864c3
Naoya Hatta <dalance@gmail.com>
2020-12-25 15:27:27 +0900

    Fix test type
    Signed-off-by: Naoya Hatta <dalance@gmail.com>

--- tests/generic/member/class_member_test_5.sv
+++ tests/generic/member/class_member_test_5.sv
@@ -3,6 +3,7 @@
 :description: Test
 :should_fail_because: pure virtual methods can only be declared in virtual classes
 :tags: 8.3
+:type: simulation
 */
 class myclass;
 pure virtual task pure_task1;
